44642abcb83dacdde4c4da92531a2ce771adf01b
Kelvin-Kimani <kelvinkim996@gmail.com>
2022-10-07 17:20:57 +0300

    Added work experience

--- src/constants/experiences.js
+++ src/constants/experiences.js
@@ -7,6 +7,18 @@ export default [
                     "company" : "Tracom Services Limited",
                     "desc" : "Developed Web solutions using Spring boot",
                     "startDate" : "August 2021",
+                    "endDate" : "March 2022"   
+                }
+            ]
+    },
+        {
+        "year": 2022,
+        "works": [
+                {
+                    "name" : "Software Engineer (Backend and DevOps)",
+                    "company" : "Presta Capital Limited",
+                    "desc" : "Developed a digital lending and guarantorship platform for lending enterprises (SACCOs and Microfinances)",
+                    "startDate" : "April 2022",
                     "endDate" : "Present"   
                 }
             ]
